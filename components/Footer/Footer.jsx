4aa199e82dcaeadfb9216160331d8320707207d5
Julie Williams <jwilliams137.036@gmail.com>
2025-02-11 20:54:31 -0500

    added footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
+import styles from './Footer.module.css'
 
 function Footer() {
+  const getCurrentYear = () => {
+    return new Date().getFullYear()
+  }
+
+  const copyrightStatement = `© ${getCurrentYear()} Linda Atkinson. All rights reserved.`
+
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+      </div>
+      <span className={styles.copyright}>{copyrightStatement}</span>
+    </footer>
   )
 }
 
