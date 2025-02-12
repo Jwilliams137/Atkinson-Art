450c9a79d56faf44a778ff9f9cc605ce9858a963
Julie Williams <jwilliams137.036@gmail.com>
2025-02-12 10:01:54 -0500

    social links

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
-import Link from 'next/link'
-import styles from './Footer.module.css'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import styles from './Footer.module.css';
 
 function Footer() {
   const getCurrentYear = () => {
-    return new Date().getFullYear()
+    return new Date().getFullYear();
   }
 
-  const copyrightStatement = `© ${getCurrentYear()} Linda Atkinson. All rights reserved.`
+  const copyrightStatement = `© ${getCurrentYear()} Linda Atkinson. All rights reserved.`;
+
+  const socialLinks = [
+    { href: "https://www.facebook.com/artistlindaatkinson", icon: faFacebook },
+    { href: "https://www.instagram.com/linda.atkinson111/", icon: faInstagram },
+  ];
 
   return (
     <footer className={styles.footer}>
       <div className={styles.links}>
+        {socialLinks.map(({ href, icon }) => (
+          <a
+            key={href}
+            href={href}
+            className={styles.navlink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              className={styles.socialLink}
+              color="#2e2c2e"
+              icon={icon}
+              size="xl"
+            />
+          </a>
+        ))}
       </div>
+
       <span className={styles.copyright}>{copyrightStatement}</span>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
