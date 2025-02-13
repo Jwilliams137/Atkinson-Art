738656cee3d57f7b05b28bcb1f9893f85c44af30
Julie Williams <jwilliams137.036@gmail.com>
2025-02-13 14:47:34 -0500

    subpages added to navData.json

---
+++ src/app/about/bio/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./page.module.css"
+
+function page() {
+  return (
+    <div className={styles.main}>
+        <h2>Bio</h2>
+    </div>
+    
+  )
+}
+
+export default page
