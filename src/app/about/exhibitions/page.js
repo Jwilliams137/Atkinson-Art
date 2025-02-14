c48e9bd43853411897ffab3486c5432ea5b25248
Julie Williams <jwilliams137.036@gmail.com>
2025-02-13 21:45:54 -0500

    styles

--- src/app/about/exhibitions/page.js
+++ src/app/about/exhibitions/page.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './page.module.css'
 
 function page() {
   return (
-    <div>
+    <div className={styles.main}>
         <h2>Exhibitions</h2>
     </div>
   )
