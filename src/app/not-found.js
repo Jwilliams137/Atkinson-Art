1e0ba8638867006cea3274b03db4274acc2ae47b
Julie Williams <jwilliams137.036@gmail.com>
2025-02-15 13:53:59 -0500

    linked fixed

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./not-found.module.css";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>404 - Page Not Found</h2>
       <p className={styles.text}>Oops! We couldn’t find that page.</p>
-      <a href="/" className={styles.button}>Go Home</a>
+      <Link><p href="/" className={styles.button}>Go Home</p></Link>
+      
     </div>
   );
 }
