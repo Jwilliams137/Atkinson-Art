fb77198d5b864b13de56f382601a3a108d2db0ad
Julie Williams <jwilliams137.036@gmail.com>
2025-03-03 16:33:32 -0500

    modal

--- src/app/artwork/new-work/page.js
+++ src/app/artwork/new-work/page.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from "react"; // Import useState
+import React, { useState, useEffect } from "react"; // Import useState, useEffect
 import usePageImages from "../../../hooks/usePageImages";
 import ImageGallery from "../../../components/ImageGallery/ImageGallery";
 import Modal from "../../../components/Modal/Modal";  // Import the Modal component
@@ -11,6 +11,7 @@ const NewWorkPage = () => {
   // State for controlling the modal visibility and selected image index
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth); // Track window width
 
   // Function to open the modal and set the selected image index
   const openModal = (index) => {
@@ -23,6 +24,17 @@ const NewWorkPage = () => {
     setIsModalOpen(false);
   };
 
+  // Update window width on resize
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // Only allow the modal to be rendered if the screen width is large enough
+  const shouldRenderModal = windowWidth > 1000;
+
   return (
     <div className={styles.newWorkContainer}>
       <ImageGallery
@@ -33,8 +45,8 @@ const NewWorkPage = () => {
         onImageClick={openModal} // Pass openModal function to ImageGallery
       />
 
-      {/* Conditionally render the Modal */}
-      {isModalOpen && (
+      {/* Conditionally render the Modal based on screen width */}
+      {isModalOpen && shouldRenderModal && (
         <Modal
           images={newWorkImages}
           currentImageIndex={currentImageIndex}
