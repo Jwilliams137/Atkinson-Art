9800055a71e5333ca6855d23e3db6ef1b71b8223
Julie Williams <jwilliams137.036@gmail.com>
2025-03-11 11:29:13 -0400

    warning fixed

--- src/app/artwork/page.js
+++ src/app/artwork/page.js
@@ -8,9 +8,26 @@ import useTextUploads from "../../hooks/useTextUploads";
 import TextSection from "../../components/TextSection/TextSection";
 import styles from './page.module.css';
 
+// Move sortImagesByPageLinks function outside the component
+const sortImagesByPageLinks = (images, pageLinks) => {
+  const pageLinkTitles = Object.keys(pageLinks);
+  return images.sort((a, b) => {
+    return pageLinkTitles.indexOf(a.title) - pageLinkTitles.indexOf(b.title);
+  });
+};
+
 const ArtworkPage = () => {
   const [images, setImages] = useState([]);
   const artworkTextUploads = useTextUploads("artwork");
+
+  // Define pageLinks inside the component
+  const pageLinks = {
+    'Click to visit the New Work page': '/artwork/new-work',
+    'Click to visit the Houses page': '/artwork/houses',
+    'Click to visit the Constructions page': '/artwork/constructions',
+    'Click to visit the Collage page': '/artwork/collage',
+    'Click to visit the Earlier Work page': '/artwork/earlier-work'
+  };
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -25,7 +42,7 @@ const ArtworkPage = () => {
           }
         });
 
-        const sortedImages = sortImagesByPageLinks(fetchedImages);
+        const sortedImages = sortImagesByPageLinks(fetchedImages, pageLinks);
         setImages(sortedImages);
       } catch (error) {
         console.error('Error fetching images:', error);
@@ -33,22 +50,7 @@ const ArtworkPage = () => {
     };
 
     fetchImages();
-  }, []);
-
-  const pageLinks = {
-    'Click to visit the New Work page': '/artwork/new-work',
-    'Click to visit the Houses page': '/artwork/houses',
-    'Click to visit the Constructions page': '/artwork/constructions',
-    'Click to visit the Collage page': '/artwork/collage',
-    'Click to visit the Earlier Work page': '/artwork/earlier-work'
-  };
-
-  const sortImagesByPageLinks = (images) => {
-    const pageLinkTitles = Object.keys(pageLinks);
-    return images.sort((a, b) => {
-      return pageLinkTitles.indexOf(a.title) - pageLinkTitles.indexOf(b.title);
-    });
-  };
+  }, []);  // No need to include sortImagesByPageLinks in the dependency array
 
   return (
     <div className={styles.artworkPage}>
