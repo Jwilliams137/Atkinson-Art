02e1b652d1acf06e4a8f2f42a61a3cc4a6989ba1
Julie Williams <jwilliams137.036@gmail.com>
2025-04-02 05:18:32 -0400

    contact switched

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 function ContactPage() {
   return (
     <div className={styles.container}>
-      <form action="https://formsubmit.co/jwilliams137.036@gmail.com" method="POST" className={styles.form}>
+      <form action="https://formsubmit.co/Linda.atkinson111@gmail.com" method="POST" className={styles.form}>
         <input 
           type="text" 
           name="name" 
