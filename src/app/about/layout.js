4846e7969ae7ff8178db165c5c1d25929987a4f8
Julie Williams <jwilliams137.036@gmail.com>
2025-05-01 05:34:00 -0400

    styles

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -4,10 +4,12 @@ import styles from './layout.module.css';
 export default function AboutLayout({ children }) {
     return (
         <div className={styles.container}>
-            <div className={styles.sidebar}>
+            <aside className={styles.sidebar}>
                 <Sidebar pageKey="about" />
-            </div>
-            <main className={styles.main}>{children}</main>
+            </aside>
+            <main className={styles.main}>
+                {children}
+            </main>
         </div>
     );
 }
