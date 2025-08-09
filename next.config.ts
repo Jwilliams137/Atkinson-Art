49b72a64111f2f9152f594a51a191edaa0d2d075
Julie Williams <jwilliams137.036@gmail.com>
2025-08-09 06:26:51 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
+      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
     ],
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [360, 414, 768, 1000, 1200],
+    imageSizes: [60, 120, 320, 600],
   },
   async redirects() {
     return [
-      {
-        source: '/index.php',
-        destination: '/',
-        permanent: true,
-      },
+      { source: "/index.php", destination: "/", permanent: true },
     ];
   },
 };
